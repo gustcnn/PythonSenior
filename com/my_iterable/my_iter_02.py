f1b23fd5719f494376dd9b07bf1ccfbe0f61b5d2
cnn <729481944@qq.com>
2019-01-11 20:37:55 +0800

    迭代器

---
+++ com/my_iterable/my_iter_02.py
@@ -0,0 +1,35 @@
+# --*--coding:utf-8
+# Author:cnn
+from collections import Iterable
+
+
+class Classmate(object):
+    def __init__(self):
+        # 定义一个对象属性
+        self.names = list()
+
+    def add(self, name):
+        """增加姓名"""
+        self.names.append(name)
+    #TODO
+    def __iter__(self):
+        """重写迭代方法,Classmate对象是可迭代的"""
+        return ClassmateIterator(self)
+
+
+class ClassmateIterator(object):
+    def __iter__(self,obj):
+        self.obj=obj
+
+    def __next__(self):
+        pass
+
+
+if __name__ == '__main__':
+    classmate = Classmate()
+    classmate.add("kobe")
+    classmate.add("lbj")
+    classmate.add("allen")
+    print("验证classmate对象是否是可迭代的:", isinstance(classmate, Iterable))
+    for name in classmate.names:
+        print(name)
